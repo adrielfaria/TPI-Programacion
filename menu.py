902c93aa9778795e1f83780acb811f68ec83f3c6
Adriel <adrielfa82@gmail.com>
2026-06-12 17:01:02 -0300

    Agregar ejecucion de funciones al menu principal

--- menu.py
+++ menu.py
@@ -1,17 +1,44 @@
 from validaciones import pedir_numero   # Se importa la función pedir_numero desde el módulo validaciones
+from operaciones import agregar_pais, actualizar_pais, buscar_pais
+from filtros import filtrar_paises_por
+from ordenar import ordenar_paises
+from estadisticas import mostrar_estadisticas
+from archivo import leer_filas
 
+def salir():
+    print('Terminando la ejecuccion del programa')
+    return False
 
 def mostrar_menu():
+    opciones = {
+        '1': lambda: agregar_pais(leer_filas()),
+        '2': lambda: actualizar_pais(leer_filas()),
+        '3': lambda: buscar_pais(leer_filas()),
+        '4': filtrar_paises_por,
+        '5': ordenar_paises,
+        '6': lambda: mostrar_estadisticas(leer_filas()),
+        '7': salir
+    }
+    while True:
+        print("\n===== GESTIÓN DE PAÍSES =====")
+        print("1) Agregar país")
+        print("2) Actualizar país")
+        print("3) Buscar país")
+        print("4) Filtrar países")
+        print("5) Ordenar países")
+        print("6) Mostrar estadísticas")
+        print("7) Salir")
 
-    print("\n===== GESTIÓN DE PAÍSES =====")
-    print("1) Agregar país")
-    print("2) Actualizar país")
-    print("3) Buscar país")
-    print("4) Filtrar países")
-    print("5) Ordenar países")
-    print("6) Mostrar estadísticas")
-    print("0) Salir")
+        opcion = pedir_numero("\nSeleccione una opción: ")
 
-    opcion = pedir_numero("\nSeleccione una opción: ")
+        funcion = opciones.get(opcion)
 
-    return int(opcion)
+        if funcion:
+            resultado = funcion()
+            if resultado is False:
+                break
+
+        else: print('Opcion Invalida')
+
+
+mostrar_menu()
